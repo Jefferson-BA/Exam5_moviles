f8f45b3e46aed96d2bfa045af23e16657e5980a8
Jefferson <jefferson.ig.ig@gmail.com>
2025-11-14 16:31:17 -0500

    creacion del AppDatabase.kt

--- app/src/main/java/com/tecsup/bautista_semana5/data/local/AppDatabase.kt
+++ app/src/main/java/com/tecsup/bautista_semana5/data/local/AppDatabase.kt
@@ -1,4 +1,33 @@
 package com.tecsup.bautista_semana5.data.local
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Producto::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun productoDao(): ProductoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "productos_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
