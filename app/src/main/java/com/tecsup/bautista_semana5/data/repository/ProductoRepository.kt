91802b42dd167715c3adc9855b23b33b946b9212
Jefferson <jefferson.ig.ig@gmail.com>
2025-11-14 16:35:29 -0500

    creacion de repositorio del producto

--- app/src/main/java/com/tecsup/bautista_semana5/data/repository/ProductoRepository.kt
+++ app/src/main/java/com/tecsup/bautista_semana5/data/repository/ProductoRepository.kt
@@ -1,4 +1,33 @@
 package com.tecsup.bautista_semana5.data.repository
 
-class ProductoRepository {
+import com.tecsup.bautista_semana5.data.local.Producto
+import com.tecsup.bautista_semana5.data.local.ProductoDao
+
+class ProductoRepository(
+    private val productoDao: ProductoDao
+) {
+
+    suspend fun listarProductos(query: String): List<Producto> {
+        return if (query.isBlank()) {
+            productoDao.getAll()
+        } else {
+            productoDao.search(query)
+        }
+    }
+
+    suspend fun insertar(producto: Producto) {
+        productoDao.insert(producto)
+    }
+
+    suspend fun actualizar(producto: Producto) {
+        productoDao.update(producto)
+    }
+
+    suspend fun eliminar(producto: Producto) {
+        productoDao.delete(producto)
+    }
+
+    suspend fun obtenerPorCodigo(codigo: String): Producto? {
+        return productoDao.getByCodigoBarras(codigo)
+    }
 }
